7d65676b50e1d66d17da6464ba4dc3350fc2d54f
Mohanad <mohanned.3262@gmail.com>
2024-05-11 18:32:45 +0300

    Schedule pick-up in Notifications in Organization

--- Kindle/src/Organization/card.js
+++ Kindle/src/Organization/card.js
@@ -2,24 +2,25 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const popupContainer = document.querySelector(".popup-container");
     const closeButton = document.querySelector(".close-button");
-    const scheduleButton = document.querySelectorAll(".btn-primary");
+    const scheduleButton = document.querySelectorAll(".btn-sm");
     const schButton = document.getElementById("schButton");
 
     scheduleButton.forEach(button => {
         button.addEventListener('click', function() {
-
             popupContainer.style.display = "block";
-            
+            $('.overlay').addClass('active');
         });
     });
 
     closeButton.addEventListener("click", function() {
         popupContainer.style.display = "none";
+        $('.overlay').removeClass('active');
         });
     schButton.addEventListener("click", function() {
         popupContainer.style.display = "none";
+        $('.overlay').removeClass('active');
         Swal.fire({
-            title: "Good job!",
+            title: "Well Done!",
             text: "Pick-up Scheduled Successfully",
             icon: "success",
             showConfirmButton: false,
